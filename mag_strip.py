79c7b81177c13a5c6b1d5574bc437be9a7f77677
Max Howell <max.howell@chem.ox.ac.uk>
2017-03-23 13:34:37 +0000

    changed mag_strip such that it can handle the output of the new code

--- mag_strip.py
+++ mag_strip.py
@@ -6,7 +6,7 @@ import numpy as np
 if not os.path.isdir(os.getcwd()+"/mag_files"):
     os.makedirs(os.getcwd()+"/mag_files")
 
-for f in glob.glob(os.getcwd()+"/B_*/MU_*.*/output*"):
+for f in glob.glob(os.getcwd()+"/B_*/MU_*.*/output_*"):
     beta = float(f.split("/")[-1].split("_")[3])
     mu = float(f.split("/")[-1].split("_")[4][:-4])
 
@@ -15,4 +15,5 @@ for f in glob.glob(os.getcwd()+"/B_*/MU_*.*/output*"):
     if os.path.isfile(os.getcwd()+"/mag_files/mag_"+str(beta)+"_"+str(mu)+".dat"):
         os.remove(os.getcwd()+"/mag_files/mag_"+str(beta)+"_"+str(mu)+".dat")
 
-    os.system("cat "+f+" | grep Mag | awk '{print $2}' >> mag_files/mag_"+str(beta)+"_"+str(mu)+".dat")
+    #os.system("cat "+f+" | grep Mag | awk '{print $2}' >> mag_files/mag_"+str(beta)+"_"+str(mu)+".dat")
+    os.system("cat "+f+" | grep Sz | awk '{print 2.0*$8}' >> mag_files/mag_"+str(beta)+"_"+str(mu)+".dat")

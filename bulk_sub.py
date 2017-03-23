8664e99ebb39129ff13a795c8411cd05fdef8725
Max Howell <max.howell@chem.ox.ac.uk>
2017-03-23 13:34:10 +0000

    Changed bulk_sub such that it can handle the command line options of the new code

--- bulk_sub.py
+++ bulk_sub.py
@@ -3,11 +3,14 @@ import numpy as np
 from shutil import copy, copyfile
 import os
 
-SUBMIT_SCRIPT='/home/dirac/tmcs/bras37867/CTHYB_SEGMENT/executables/runs/FCC_hubbard_t_prime/test/submit_DMFT.sh'
+#SUBMIT_SCRIPT='/home/dirac/tmcs/bras37867/CTHYB_SEGMENT/executables/runs/FCC_hubbard_t_prime/test/submit_DMFT.sh'
+SUBMIT_SCRIPT='/home/dirac/tmcs/bras37867/CTHYB_SEGMENT/executables/runs/FCC_hubbard_t_prime/U_21_restart/long_cycle_length/l_200/submit_DMFT.sh'
 SUBMIT_SCRIPT_NAME = 'submit_DMFT.sh'
 
 mu_list = [14.0]
-beta_list = np.arange(6.25,7.25,0.25)
+#beta_list = [8.5,9.0]
+#beta_list = []
+beta_list = np.arange(6.0,7.0,0.1)
 
 print "beta vals: ", beta_list
 print "mu vals: ", mu_list
@@ -33,12 +36,14 @@ for beta in beta_list:
 
         # Copy what is needed to the new directory
         copy(cwd+'/anderson_FCC',directory)
-        copy(cwd+'/fcc_dos.dat',directory)
+        #copy(cwd+'/fcc_dos.dat',directory)
+        copy(cwd+'/dos.dat',directory)
 
         # Change to the new directory and submit the job
         os.chdir(directory)
         #print 'qsub ' + SUBMIT_SCRIPT_NAME + ' --U 21.0 --mu ' + str(mu) + ' --beta ' + str(beta) + ' --n_cycles 15000000 --warmup_cycles 1000000 --length_cycle 10 --dmft_loops 30 --n_g_l 40 --do_dmft'
-        os.system('qsub ' + SUBMIT_SCRIPT_NAME + ' --U 21.0 --mu ' + str(mu) + ' --beta ' + str(beta) + ' --n_cycles 15000000 --warmup_cycles 1000000 --length_cycle 10 --dmft_loops 100 --n_g_l 35 --do_dmft' )
+        os.system('qsub ' + SUBMIT_SCRIPT_NAME + ' --U 21.0 --mu ' + str(mu) + ' --beta ' + str(beta) + ' --n_cycles 10000000 --warmup_cycles 1000000 --length_cycle 200 --dmft_loops 50 --n_g_l 35 --do_dmft --no_enforce_spin_symm --n_vertex_cycles 20000000 --legendre_chi --n_g4_l 35 ')
+        #os.system('qsub ' + SUBMIT_SCRIPT_NAME + ' --U 21.0 --mu ' + str(mu) + ' --beta ' + str(beta) + ' --n_cycles 40000000 --warmup_cycles 1000000 --length_cycle 10 --dmft_loops 100 --n_g_l 35 --do_dmft --no_enforce_spin_symm --legendre_chi' )
 
         #change back to cwd
         os.chdir(cwd)
